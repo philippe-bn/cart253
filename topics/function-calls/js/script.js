1a751180f2f2f7a4c71dadc87547bc131a1911eb
philippe-bn <83093735+philippe-bn@users.noreply.github.com>
2025-09-08 21:43:37 -0400

    Added comments for the Commenting module in my Sailboat mini project

--- topics/function-calls/js/script.js
+++ topics/function-calls/js/script.js
@@ -2,13 +2,21 @@
  * Sailboat
  * Philippe Beauchemin
  * 
- * A view from above of a boat out at sea on the Atlantic.
+ * A view from above of a boat out at sea on the Atlantic, 
+ * meant to emulate in very little detail the Romantic depiction 
+ * of nature as an overpowering force to be reckoned with. 
+ * The striking visual contrast between the size of the boat 
+ * and its surroundings is meant to provoke a feeling of the Sublime.
+ * 
+ * * Uses:
+ * p5.js
+ * https://p5js.org/
  */
 
 "use strict";
 
 /**
- * Create a canvas to draw on.
+ * Create a 900x500 horizontal canvas to draw our landscape
 */
 function setup() {
     createCanvas(900, 500);
@@ -16,9 +24,11 @@ function setup() {
 
 
 /**
- * Draw the sea and boat.
+ * Draws the sea and the boat
 */
 function draw() {
+    // The Atlantic
     background(50, 50, 255);
+    // The sailboat
     rect(100, 100, 30, 70);
 }
