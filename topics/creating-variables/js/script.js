14055388e9f92b1071db849552021a63d3b47329
philippe-bn <83093735+philippe-bn@users.noreply.github.com>
2025-09-15 21:29:59 -0400

    Followed along creating-variables module

--- topics/creating-variables/js/script.js
+++ topics/creating-variables/js/script.js
@@ -11,14 +11,30 @@
 /**
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
-function setup() {
+// Our cheese colour broken into RGB
+let cheeseRed = 255;
+let cheeseGreen = 255;
+let cheeseBlue = 255;
 
+// Our cheese hole
+let holeShade = 0; // Greyscale value for the hole
+let holeX = 140; // x-coordinate of the hole
+let holeY = 175; // y-coordinate of the hole
+let holeSize = 180; // Diameter of the hole
+
+function setup() {
+    // Create the canvas
+    createCanvas(480, 480);
 }
 
-
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
 function draw() {
-
+    // Cheese colour (yellow)
+    background(cheeseRed, cheeseGreen, cheeseBlue);
+    
+    // Draw a hole in the upper left
+    push();
+    noStroke();
+    fill(holeShade);
+    ellipse(holeX, holeY, holeSize);
+    pop();
 }
