a0c6ee4a6c2c8683ab58bcaf1855b13eb0b88120
philippe-bn <83093735+philippe-bn@users.noreply.github.com>
2025-09-29 21:34:02 -0400

    Completed key events module

---
+++ topics/events/keyboard-events/js/script.js
@@ -0,0 +1,75 @@
+/**
+ * Keyboard Events
+ * Pippin Barr
+ * 
+ * A chance to experiment with keyboard events in a simple setting.
+*/
+
+"use strict";
+
+// Our ball
+const ball = {
+    // Position
+    x: 200,
+    y: 200,
+    // Size
+    size: 50,
+    // fill
+    fill: "#ffffff",
+    // fills
+    fills: {
+        white: "#ffffff",
+        red: "#ff0000",
+        blue: "#0000ff"
+    },
+    // Keys that control the ball's colour
+    keys: {
+        redKey: 82, // R
+        blueKey: 66 // B
+    }
+}
+
+/**
+ * Creates the canvas
+ */
+function setup() {
+    createCanvas(400, 400);
+}
+
+/**
+ * Draws the ball
+ */
+function draw() {
+    background(0);
+
+    // Draw the ball
+    push();
+    noStroke();
+    fill(ball.fill);
+    ellipse(ball.x, ball.y, ball.size);
+    pop();
+}
+
+/**
+ * Handle keypress:
+ * - R = make the ball red
+ * - B = make the ball blue
+ */
+function keyPressed(event) {
+    if (event.keyCode === ball.keys.redKey) {
+        ball.fill = ball.fills.red;
+    }
+    else if (event.keyCode === ball.keys.blueKey) {
+        ball.fill = ball.fills.blue;
+    }
+}
+
+/**
+ * Handle keyreleased:
+ * - R or B = Set the ball back to default
+ */
+function keyReleased(event) {
+    if (event.keyCode === ball.keys.redKey || event.keyCode === ball.keys.blueKey) {
+        ball.fill = ball.fills.white;
+    }
+}
