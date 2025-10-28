ed7222c36f1c9d225116be2e3db3db456336aaee
philippe-bn <83093735+philippe-bn@users.noreply.github.com>
2025-10-28 11:44:25 -0400

    Finished sequential arrays module

--- topics/arrays/speech-speech/js/script.js
+++ topics/arrays/speech-speech/js/script.js
@@ -1,24 +1,31 @@
-/**
- * Title of Project
- * Author Name
- * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
- */
+let speech = ["Veni.", "Vidi.", "Vici.", "Sensi malum."];
+let speechIndex = 0;
 
-"use strict";
-
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
 function setup() {
-
+    createCanvas(600, 100);
 }
 
-
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
 function draw() {
+    background(0);
 
+    // Get the current line of our speech
+    let currentLine = speech[speechIndex];
+
+    // Display it
+    push();
+    fill(255);
+    textSize(32);
+    textAlign(CENTER, CENTER); // Centered horizontally and vertically
+    text(currentLine, width / 2, height / 2); // Centered on the canvas
+    pop();
+}
+
+function mousePressed() {
+    speechIndex = speechIndex + 1;
+    // Or: speechIndex += 1;
+    // Or: speechIndex++;
+    if (speechIndex >= speech.length) {
+        // If so, reset it to 0 (go back to the beginning)
+        speechIndex = 0;
+    }
 }
