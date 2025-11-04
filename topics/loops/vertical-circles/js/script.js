ce55031b3f8eb8733c9e0b343442fa14ff38fffb
philippe-bn <83093735+philippe-bn@users.noreply.github.com>
2025-11-04 11:58:00 -0500

    Finished intro to while loops

--- topics/loops/vertical-circles/js/script.js
+++ topics/loops/vertical-circles/js/script.js
@@ -21,16 +21,17 @@ function setup() {
 function draw() {
     background(0);
 
-    // Draw a series of 50-pixel diameter circles
-    // Starting at the top of the canvas
-    // And ending at the bottom
-    ellipse(200, 0, 50);
-    ellipse(200, 50, 50);
-    ellipse(200, 100, 50);
-    ellipse(200, 150, 50);
-    ellipse(200, 200, 50);
-    ellipse(200, 250, 50);
-    ellipse(200, 300, 50);
-    ellipse(200, 350, 50);
-    ellipse(200, 400, 50);
+    // Set up the position and diameter of the first circle
+    let x = width / 2;
+    let y = 0;
+    let diameter = 10;
+
+    // Here is the magical while loop
+    // Keep checking if y is still less than the height...
+    while (y <= height) {
+        // If it is, draw the next circle
+        ellipse(x, y, diameter);
+        // And increase y to move down
+        y += diameter;
+    }
 }
