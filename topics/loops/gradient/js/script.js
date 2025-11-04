898742943115c2a3bc777a52d6ed5072c1d443e9
philippe-bn <83093735+philippe-bn@users.noreply.github.com>
2025-11-04 12:31:18 -0500

    Finished for loops practice 2

--- topics/loops/gradient/js/script.js
+++ topics/loops/gradient/js/script.js
@@ -12,13 +12,26 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
+    createCanvas(400, 400);
 }
 
-
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
 function draw() {
+    background('white');
 
+    // Here's our loop that goes from 0 (the left) to width (the right)
+    // of the canvas. Let's call it x because that's what it is!
+    // x stars at 0 (the left) and goes up by one (x++) until it reaches
+    // the width
+    for (let x = 0; x <= width; x += 5) {
+        // Each time, we will draw a line vertically on the canvas at x
+        push();
+        // We will choose a colour for the line by mapping the current x
+        // Which is between 0 and width
+        // To a valid fill value, which is between 0 and 255
+        let strokeColour = map(x, 0, width, 0, 255);
+        stroke(strokeColour);
+        // Draw the line
+        line(x, 0, x, height);
+        pop();
+    }
 }
