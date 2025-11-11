0daf0caf1308e2c611940ce79c018943c92a31dd
philippe-bn <83093735+philippe-bn@users.noreply.github.com>
2025-11-11 11:22:01 -0500

    Finished tarot data module

--- topics/data/tarot/js/script.js
+++ topics/data/tarot/js/script.js
@@ -7,17 +7,33 @@
 
 "use strict";
 
-/**
- * tbd.
-*/
-function setup() {
+// A global variable to store our data in
+let tarot;
+// A global variable to store our fortune in
+let fortune;
 
+function preload() {
+    // Used in preload, loadJSON will just return the data into our variable
+    tarot = loadJSON("assets/data/tarot_interpretations.json");
 }
 
+function setup() {
+    createCanvas(windowWidth, windowHeight);
 
-/**
- * tbd.
-*/
+    // Choose a random card
+    let card = random(tarot.tarot_interpretations);
+    // Choose a random fortune
+    fortune = random(card.fortune_telling);
+}
+
 function draw() {
+    background(0);
 
+    // Display the fortune
+    push();
+    textSize(18);
+    textAlign(CENTER, CENTER);
+    fill(255, 255, 0);
+    text(fortune, width / 2, height / 2);
+    pop();
 }
